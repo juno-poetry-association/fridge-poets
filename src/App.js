160d871803614b737f38b58ef961ab05a0d5fa4e
Charlotte <charlottecboyer@gmail.com>
2021-07-30 16:24:35 -0400

    api tested & clear

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
 import './App.css';
+import { useEffect, useState } from 'react'; 
+import axios from 'axios';
 
 function App() {
+
+  useEffect( () => {
+    axios ({
+      url: "https://api.datamuse.com/words",
+      method: 'GET',
+      dataResponse: 'json',
+      params: {
+        rel_jjb: 'ocean',
+      }
+      
+    }).then((res)=>{
+      console.log(res.data);
+    })
+  },[])
+
   return (
     <div>
       <h1>POETRY</h1>
