d3a61a9a74dd2f75c872f2957a7e16cf337fe75c
Kyle <kylerobertduncan@gmail.com>
2021-08-05 16:00:53 -0400

    console warning resolved

--- src/Form.js
+++ src/Form.js
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function Form(props) {
   const { setUserSearchTerm, setFunctionsDisplayed } = props;
