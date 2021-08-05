0c85c86cbd74ebf05f22c37c1200322d9c529e76
Sakina <sakina.nadiadi@gmail.com>
2021-08-05 17:28:00 -0400

    done with commenting on Form.js, FunctionWords.js and SavedPoem.js

--- src/FunctionWords.js
+++ src/FunctionWords.js
@@ -1,11 +1,12 @@
-
 const FunctionWords = (props) => {
+  // Getting the props from the App.js
   const { yourPoem, setYourPoem, setFunctionArray, functionArray } = props;
 
   function addWordToPoem(word) {
+    // making copy of an poem array
     const newPoem = [...yourPoem, word];
     setYourPoem(newPoem);
-    // TO REMOVE WORD
+    // TO REMOVE WORD from the functionWordArray
     const oldFunctionsArr = [...functionArray];
     const filteredArr = oldFunctionsArr.filter((arrWord) => arrWord !== word);
     setFunctionArray(filteredArr);
@@ -14,7 +15,7 @@ const FunctionWords = (props) => {
   // keyboard accessibility to add words to poem
   function handleKeyPress(e, word) {
     if (e.charCode === 13) {
-      addWordToPoem(word)
+      addWordToPoem(word);
     }
   }
 
@@ -22,7 +23,12 @@ const FunctionWords = (props) => {
     <ul>
       {functionArray.map((word) => {
         return (
-          <li key={word} onClick={() => addWordToPoem(word)} onKeyPress={(e) => handleKeyPress(e, word)} tabIndex="0">
+          <li
+            key={word}
+            onClick={() => addWordToPoem(word)}
+            onKeyPress={(e) => handleKeyPress(e, word)}
+            tabIndex="0"
+          >
             <p>{word}</p>
           </li>
         );
